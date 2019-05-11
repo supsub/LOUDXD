2fff4667ae7e984ed4ffae90c900ee0b0c2491c5
milowedo <miloszszwedo@gmail.com>
2019-05-11 18:05:51 +0200

    fixed assignment problem

--- src/lexer.py
+++ src/lexer.py
@@ -12,43 +12,42 @@ class Lexer():
         # Parenthesis
         self.lexer.add('OPEN_PAREN', r'\(')
         self.lexer.add('CLOSE_PAREN', r'\)')
-        # command separator
+        # Semi Colon
         self.lexer.add('DOT', r'\.')
-        # argument separator
-        self.lexer.add("COMMA", r'\,')
-        # Operators
+        # Operators +,-
         self.lexer.add('SUM', r'\+')
         self.lexer.add('SUB', r'\-')
-        # variable name like response_time or latencySegID
-        self.lexer.add('VARIABLE', r'(_|[a-zA-Z])(_|[a-zA-Z]|[0-9])*')
         # Number
         self.lexer.add('NUMBER', r'\d+')
-        # # Variable(as a type, so int, string, number etc)
-        # self.lexer.add('VARIABLE', r'Zmienna')
         # ++
         self.lexer.add('INCREMENT', r'Zwiększ')
         # --
         self.lexer.add('DECREMENT', r'Zmniejsz')
-        # +/-
+        # +,- operations helper 
         self.lexer.add('ADDSUB_HELPER', r'o')
-         # * /
+        # *,/ operations helper
         self.lexer.add('DIVMUL_HELPER', r'przez')
-        # assignment 
+        # assignment
         self.lexer.add('ASSIGN', r'jest równe')
-        # string
+        
+        #variable name, ex. response_time or latencySegID  
+        self.lexer.add('VARIABLE', r'(_|[a-zA-Z])(_|[a-zA-Z]|[0-9])*')
+
         self.lexer.add("STRING", r'\"[^\"]*\"')
+
+        self.lexer.add("COMMA", r'\,')
         
         ###
         #comparisons
         ###
         #bigger >
-        # self.lexer.add('BIGGER', r'jest większe')
-        # #smaller <
-        # self.lexer.add('SAMLLLER', r'jest mniejsze')
-        # #equal ==
-        # self.lexer.add('EQUAL', r'równa się')
-        # #!= differ
-        # self.lexer.add('DIFFER', r'jest różne od')
+        self.lexer.add('BIGGER', r'jest większe')
+        #smaller <
+        self.lexer.add('SAMLLLER', r'jest mniejsze')
+        #equal ==
+        self.lexer.add('EQUAL', r'równa się')
+        #!= differ
+        self.lexer.add('DIFFER', r'jest różne od')
         
         ###
         #if
@@ -57,6 +56,7 @@ class Lexer():
         ###
         #for
         ###
+
 
 
         # Ignore spaces
